db748df5d6d57b861e73c930317906a3dfba3004
Franssen <ngoiefranssen04@gmail.com>
2022-12-07 09:03:28 +0200

    Wip

--- react_frontend/src/index.js
+++ react_frontend/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = ReactDOM.createRoot(document.getElementById('root'));
