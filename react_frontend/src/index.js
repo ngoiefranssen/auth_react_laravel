11eae6d5d9cee0e8a58164e23991ccc1f1e77c9a
Franssen <ngoiefranssen04@gmail.com>
2022-12-07 08:37:55 +0200

    WIp

--- react_frontend/src/index.js
+++ react_frontend/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-// import { Brow }
+import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = ReactDOM.createRoot(document.getElementById('root'));
 rootElement.render(
-  <React.StrictMode>
+
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 reportWebVitals();
