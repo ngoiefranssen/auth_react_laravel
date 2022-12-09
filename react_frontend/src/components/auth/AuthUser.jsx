59dbe4369a1b433cd8d9b5aa53e5193d0bc0f345
Franssen <ngoiefranssen04@gmail.com>
2022-12-09 16:25:37 +0200

    Wip

---
+++ react_frontend/src/components/auth/AuthUser.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import axios from "axios"
+
+export default function AuthUser() {
+
+    const http = axios.create({
+        baseURL:'http://localhost:8000/api',
+        headers:{
+            "content-type" : "application/json",
+        },
+    });
+  return (
+    <div>
+      
+    </div>
+  )
+}
