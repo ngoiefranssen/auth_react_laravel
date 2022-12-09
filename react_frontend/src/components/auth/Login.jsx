3e0a020a0699f28c0563cc04645f472c04c7ffdc
Franssen <ngoiefranssen04@gmail.com>
2022-12-09 15:45:52 +0200

    Wip

--- react_frontend/src/components/auth/Login.jsx
+++ react_frontend/src/components/auth/Login.jsx
@@ -1,40 +1,33 @@
+import { useState } from "react";
 
 
 export default function Login () {
+
+  const {email, setEmail} = useState();
+  const {password, setPassword} = useState();
+
     return(
-      <section className="vh-100" style="background-color: #508bfc;">
+      <section className="vh-90" style={{ background:"" }}>
       <div className="container py-5 h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div className="card shadow-2-strong" style="border-radius: 1rem;">
+            <div className="card shadow-2-strong" style={{ borderRadius:"1rem" }}>
               <div className="card-body p-5 text-center">
     
                 <h3 className="mb-5">Sign in</h3>
     
                 <div className="form-outline mb-4">
-                  <input type="email" id="typeEmailX-2" className="form-control form-control-lg" />
-                  <label className="form-label" for="typeEmailX-2">Email</label>
+                  <input type="email" placeholder="Enter your email...."
+                         className="form-control form-control-lg" 
+                         onChange={(e) => setPassword(e.target.value)}/>
                 </div>
     
                 <div className="form-outline mb-4">
-                  <input type="password" id="typePasswordX-2" className="form-control form-control-lg" />
-                  <label className="form-label" for="typePasswordX-2">Password</label>
+                  <input type="password" className="form-control form-control-lg"
+                         placeholder="Enter your password...."
+                         onChange={(e) => setPassword(e.target.value)}/>
                 </div>
-    
-                <div className="form-check d-flex justify-content-start mb-4">
-                  <input className="form-check-input" type="checkbox" value="" id="form1Example3" />
-                  <label className="form-check-label" for="form1Example3"> Remember password </label>
-                </div>
-    
-                <button className="btn btn-primary btn-lg btn-block" type="submit">Login</button>
-    
-                <hr className="my-4" />
-    
-                <button className="btn btn-lg btn-block btn-primary" style="background-color: #dd4b39;"
-                  type="submit"><i className="fab fa-google me-2"></i> Sign in with google</button>
-                <button className="btn btn-lg btn-block btn-primary mb-2" style="background-color: #3b5998;"
-                  type="submit"><i className="fab fa-facebook-f me-2"></i>Sign in with facebook</button>
-    
+                <button className="btn btn-outline-primary btn-lg btn-block" type="submit">Login</button>
               </div>
             </div>
           </div>
--- react_frontend/src/index.js
+++ react_frontend/src/index.js
@@ -6,6 +6,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
+
 const rootElement = ReactDOM.createRoot(document.getElementById('root'));
 rootElement.render(
 
