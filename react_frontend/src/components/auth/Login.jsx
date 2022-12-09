08214b479103dabc271ec6be3be82008cbe5e910
Franssen <ngoiefranssen04@gmail.com>
2022-12-09 15:51:40 +0200

    Wip

--- react_frontend/src/components/auth/Login.jsx
+++ react_frontend/src/components/auth/Login.jsx
@@ -6,8 +6,12 @@ export default function Login () {
   const {email, setEmail} = useState();
   const {password, setPassword} = useState();
 
+  const handlClick = () =>{
+    return email + " " + password
+  }
+
     return(
-      <section className="vh-90" style={{ background:"#FFFFFF" }}>
+      <section className="vh-90" style={{ background:"#FFFFaF" }}>
       <div className="container py-5 h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-12 col-md-8 col-lg-6 col-xl-5">
@@ -28,7 +32,7 @@ export default function Login () {
                          onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <button className="btn btn-outline-primary btn-lg btn-block"
-                        type="submit">Login</button>
+                       onClick={handlClick} type="submit">Login</button>
               </div>
             </div>
           </div>
